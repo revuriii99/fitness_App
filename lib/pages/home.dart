6fb7880e7aeff48673c7066fce93fcb243002516
revuriii99 <reviwilerii@gmail.com>
2026-06-22 10:36:56 +0700

    feat: implement layout enhancements

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Breakfast', 
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+      ),
+    );
   }
 }
