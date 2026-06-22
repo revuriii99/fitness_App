fca9a8309093cea1aac3d7e73d947664c22bbd02
revuriii99 <reviwilerii@gmail.com>
2026-06-22 23:13:48 +0700

    update ui and model

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,25 +1,66 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  void initstate() {
+    _getCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
       appBar: myAppBar(),
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _searchField(),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Category',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600
+              SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.only(left:20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              Container(
+                height: 120,
+                child: ListView.separated(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  separatorBuilder: (context, index) => SizedBox(width: 25,),
+                  itemBuilder: (context,index) {
+                    return Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: categories[index].boxColor.withOpacity(0.3)
+                      ),
+                    );
+                  },
                 ),
               )
             ]
@@ -46,7 +87,7 @@ class HomePage extends StatelessWidget {
               filled: true,
               fillColor: Colors.white,
               contentPadding: EdgeInsets.all(15),
-              hintText: 'Search Pudding',
+              hintText: 'Search Pancake',
               hintStyle: TextStyle(
                 color: Color(0xffDDDADA),
                 fontSize: 14
